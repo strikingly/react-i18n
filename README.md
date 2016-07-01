8ab621004f8c27674ba77869f53708c9a1b3605a
quietshu <ds303077135@gmail.com>
2016-07-01 09:38:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# react-i18n
+
+## Demo
+```jsx
+import {t, tct} from './src/i18n'
+
+class App extends React.Component {
+  render() => <div>
+    {
+      tct('hello! click [link:here]', {
+        root: <span/>,
+        link: <a href="#"/>
+      })
+    }
+    <hr/>
+    {
+      t('%{author}s assigned this event to %{assignee}s', {
+        author: 'example',
+        assignee: <b>example@example.com</b>
+      })
+    }
+  </div>
+}
+```
+
+## locale (using `jed`)
+```jsx
+import Jed from 'jed'
+import {setLocale} from './src/i18n'
+
+setLocale(new Jed({ /* jed options */ }))
+```
+
+## debugging mode
+```jsx
+import Jed from 'jed'
+import {setLocale} from './src/i18n'
+
+setLocaleDebug(true)
+// reload the page to apply
+```
+
+## License
+
+- [Sentry license](./LICENSE-Sentry)
+- [sprintf.js license](./LICENSE-sprintfjs)
---
+++ test/test.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import Jed from 'jed'
+import ReactDOMServer from 'react-dom/server'
+import assert from 'assert'
+
+let i18n = null
+let debug = '0'
+
+describe('i18n', () => {
+  before(() => {
+    // setup the browser environment
+    global.sessionStorage = {
+      getItem: () => debug
+    }
+    i18n = require('../src/i18n')
+  })
+
+  it('should return i18n strings or react components', () => {
+    assert(i18n.t('lorem ipsum') === 'lorem ipsum')
+    // returns 'lorem ipsum'
+
+    assert(i18n.t('%{li}s', {li: <p>lorem ipsum</p>})[0].$$typeof === Symbol.for('react.element'))
+    // returns [<p>lorem ipsum</p>]
+
+    assert(i18n.t('lorem %{li}s ipsum', {li: <p>lorem ipsum</p>}).length === 3)
+    // returns ['lorem', <p>lorem ipsum</p>, 'ipsum']
+  })
+
+  it('should return templated strings', () => {
+    assert(i18n.t('%{l}s', {l: 'lorem'}) === 'lorem');
+    assert(i18n.t('%{l}s %{i}s', {
+        l: 'lorem',
+        i: 'ipsum'
+      }) === 'lorem ipsum');
+    assert(i18n.t('%{li}j', {li: ['lorem', 'ipsum']}) === '["lorem","ipsum"]')
+
+    assert(i18n.t('%{author}s assigned this event to %{assignee}s', {
+      author: <p>example</p>,
+      assignee: <em>example@example.com</em>
+    }))
+  })
+
+  it('should return wrapped react components', () => {
+    assert(ReactDOMServer.renderToStaticMarkup(i18n.tct('lorem [li] ipsum', {
+        root: <div/>,
+        li: <b>hey</b>
+      })) === '<div><span>lorem </span><b>hey</b><span> ipsum</span></div>')
+  })
+
+  it('should translate words through jed', () => {
+    i18n.setLocale(new Jed({
+      'domain': 'i18n',
+      'missing_key_callback': function (key) {
+      },
+      'locale_data': {
+        'i18n': {
+          '': {
+            'domain': 'i18n',
+            'lang': 'fr',
+            'plural_forms': 'nplurals=2; plural=(n != 1);'
+          },
+          'hello': ['bonjour']
+        }
+      }
+    }))
+
+    assert(i18n.t('hello') === 'bonjour')
+  })
+
+  it('should return debugging wrapper', () => {
+    // set debug flag, reload module
+    delete require.cache[require.resolve('../src/i18n')]
+    debug = '1'
+    i18n = require('../src/i18n')
+
+    assert(ReactDOMServer.renderToStaticMarkup(i18n.tct('lorem [li] ipsum', {
+        root: <div/>,
+        li: <b>hey</b>
+      })) === '<span class="translation-wrapper"><div><span>lorem </span><b>hey</b><span> ipsum</span></div></span>')
+  })
+
+  it('should return some emoji chars when debugging with toString()', () => {
+    assert(i18n.tct('hello', {
+        root: <span/>
+      }).toString() === '🇦🇹[object Object]🇦🇹')
+  })
+})
